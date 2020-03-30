2825aa0054aad91783d0152fb6dc58f71dd36a55
Aaron Etienne <aetienne@scholar-fe02.rcac.purdue.edu>
2020-03-30 18:51:28 -0400

    gradescope sucks

--- program_09.py
+++ program_09.py
@@ -27,7 +27,7 @@ def ReadData( fileName ):
     DataDF = DataDF.set_index('Date')
     
     # define and initialize the missing data dictionary
-    ReplacedValuesDF = pd.DataFrame(0, index=["1. No Data", "2. Gross Error", "3. Replaced Values", "Range Fail"], columns=colNames[1:])
+    ReplacedValuesDF = pd.DataFrame(0, index=['1. No Data', '2. Gross Error', '3. Swapped', '4. Range Fail'], columns=colNames[1:])
      
     return( DataDF, ReplacedValuesDF )
  
